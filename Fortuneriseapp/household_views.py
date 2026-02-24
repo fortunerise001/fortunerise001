7ec507585bcd27a3d63b833be673d9a69b79cc2a
fortunerise001 <fortunerise001@gmail.com>
2026-02-23 22:26:54 -0500

    added image

--- Fortuneriseapp/household_views.py
+++ Fortuneriseapp/household_views.py
@@ -8,6 +8,25 @@ def list_households(request):
     households = Household.objects.all()
     return render(request, "Fortuneriseapp/household_list.html", {"households": households})
 
+def edit_household(request, pk):
+    household = get_object_or_404(Household, pk=pk)
+    PersonFormSet = inlineformset_factory(Household, Person, form=PersonForm, extra=1, can_delete=True)
+    if request.method == "POST":
+        hform = HouseholdForm(request.POST, instance=household)
+        formset = PersonFormSet(request.POST, instance=household)
+        if hform.is_valid() and formset.is_valid():
+            household = hform.save()
+            people = formset.save(commit=False)
+            for p in people:
+                p.household = household
+                p.save()
+            for deleted in formset.deleted_objects:
+                deleted.delete()
+            return redirect("household_list")
+    else:
+        hform = HouseholdForm(instance=household)
+        formset = PersonFormSet(instance=household)
+    return render(request, "Fortuneriseapp/household_form.html", {"hform": hform, "formset": formset})
 
 def create_household(request):
     PersonFormSet = inlineformset_factory(Household, Person, form=PersonForm, extra=3, can_delete=True)
